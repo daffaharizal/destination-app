e5100151ad80401047993b97857cc954948ecb81
daffaharizaleid <daffaharizal.eid@gmail.com>
2025-05-10 14:23:44 +0700

    (feat) add input shadcn

--- src/pages/setting/main.tsx
+++ src/pages/setting/main.tsx
@@ -1,7 +1,7 @@
 export default function Setting() {
   return (
     <div>
-      <h1>Setting</h1>
+      <h1 className="text-red-500 bg-cyan-500">Setting</h1>
       <p>Setting page content goes here.</p>
     </div>
   );
