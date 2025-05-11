c37ae98c4733a33b6b0107e5ea75b82558c0b6ee
daffaharizaleid <daffaharizal.eid@gmail.com>
2025-05-11 13:21:43 +0700

    (feat) config environment (.env.dev & .env.prod), configuration files and alias, and update package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env': {
+        VITE_BASE_URL: JSON.stringify(env.VITE_BASE_URL),
+      }
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@core": path.resolve(__dirname, "./src/core"),
+        "@hooks": path.resolve(__dirname, "./src/hooks"),
+      },
+    },
   }
-})
+});
